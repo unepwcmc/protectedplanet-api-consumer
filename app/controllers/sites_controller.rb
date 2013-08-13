af3be22eb651b2cff16a55ccb7f43ceedc188597
paolamastaiza@gmail.com <paolamastaiza@gmail.com>
2013-08-13 15:19:46 +0100

    http request again

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,31 +1,17 @@
 class SitesController < ApplicationController
 
-def search_by_point
-params = { :lat => 50, :lng => 20, :radius => 100.0, :type => "official", :id => 1 }
-  if params[:lat] && params[:lng]
-    params[:radius] ||= 200.0
-    geo = []
-    geo << params[:lat].to_f * Math::PI/180
-    geo << params[:lng].to_f * Math::PI/180
-    filter = {"geodist" => Range.new(0.0, params[:radius].to_f * 1000)}
-
-    type = params[:type]
-
-    @sites = Site.search 
 
 
 
-    @json = []
+def index
+    require 'httparty'
+    require 'json'
+    lat=20
+    lng=52
+    response = HTTParty.get("http://protectedplanet.net/api2/sites/search_by_point/#{lat}/#{lng}")
+    @response_body = response.body
+    @result = JSON.parse(@response_body)
 
-    @sites.each do |site|
-      @json << site.to_standards_hash({}, type)
-    end
-
-    render :json => @json.to_json
   end
-end
-
-
-
 
 end
