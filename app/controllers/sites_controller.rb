65fb4e3859354667166f08ccfc870c2e2126ae4e
paolamastaiza@gmail.com <paolamastaiza@gmail.com>
2013-08-15 11:46:00 +0100

    showing places list

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,17 +1,34 @@
 class SitesController < ApplicationController
+ require 'httparty'
+    require 'json' 
 
 
-
+ 
 
 def index
-    require 'httparty'
-    require 'json'
-    lat=20
-    lng=52
-    response = HTTParty.get("http://protectedplanet.net/api2/sites/search_by_point/#{lat}/#{lng}")
-    @response_body = response.body
-    @result = JSON.parse(@response_body)
+response= HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address=Popayan&sensor=false")
+     @response_body = response
 
+      geocoding_results = response
+
+      results = geocoding_results["results"]
+      results = results[0]
+      geometry = results["geometry"]
+      location = geometry["location"]
+      lat = location["lat"].to_i
+      lng = location["lng"].to_i
+      search_result(lat,lng)
+
+end
+
+  def search_result(lat,lng)
+
+    response = HTTParty.get("http://protectedplanet.net/api2/sites/search_by_point/#{lat}/#{lng}?type=official")
+    @results1 = JSON.parse(response.body)
   end
+
+
+
+
 
 end
