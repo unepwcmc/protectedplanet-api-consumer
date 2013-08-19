9518ca53f90eaccadd9b99f429971c564527904d
paolamastaiza@gmail.com <paolamastaiza@gmail.com>
2013-08-19 15:36:22 +0100

    creating module

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 gem 'httparty'
+gem 'pry', group: [:development]
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,12 +1,13 @@
 class SitesController < ApplicationController
  require 'httparty'
-    require 'json' 
+ require 'json' 
+ require  'PPApi'
 
-
- 
 
 def index
-response= HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address=Popayan&sensor=false")
+#binding.pry
+nombre = params[:q]
+response = HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address=Popayan&sensor=false")
      @response_body = response
 
       geocoding_results = response
@@ -17,18 +18,7 @@ response= HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address
       location = geometry["location"]
       lat = location["lat"].to_i
       lng = location["lng"].to_i
-      search_result(lat,lng)
+      @search_result= PPApi.search_result(lat,lng)
 
 end
-
-  def search_result(lat,lng)
-
-    response = HTTParty.get("http://protectedplanet.net/api2/sites/search_by_point/#{lat}/#{lng}?type=official")
-    @results1 = JSON.parse(response.body)
-  end
-
-
-
-
-
 end
