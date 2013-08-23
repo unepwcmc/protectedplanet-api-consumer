d6f1cb771c74a217828c7eccaa3115629d303522
paolamastaiza@gmail.com <paolamastaiza@gmail.com>
2013-08-23 15:16:54 +0100

    form introduction trouble

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,24 +1,16 @@
 class SitesController < ApplicationController
- require 'httparty'
- require 'json' 
- require  'PPApi'
-
-
-def index
-#binding.pry
-nombre = params[:q]
-response = HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address=Popayan&sensor=false")
-     @response_body = response
-
-      geocoding_results = response
-
-      results = geocoding_results["results"]
-      results = results[0]
-      geometry = results["geometry"]
-      location = geometry["location"]
-      lat = location["lat"].to_i
-      lng = location["lng"].to_i
-      @search_result= PPApi.search_result(lat,lng)
-
-end
+  require 'httparty'
+  require 'json' 
+  require 'PPApi'
+ 
+  def index
+    response = HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{params[:codigo]}&sensor=false")
+    @response_body = response
+    results = response["results"].first
+    geometry = results["geometry"]
+    location = geometry["location"]
+    lat = location["lat"].to_i
+    lng = location["lng"].to_i
+    @search_results=PPApi.search_result(lat,lng)
+  end
 end
--- lib/modules/PPApi.rb
+++ lib/modules/PPApi.rb
@@ -1,7 +1,5 @@
 module PPApi
-def search_result(lat,lng)
-    response = HTTParty.get("http://protectedplanet.net/api2/sites/search_by_point/#{lat}/#{lng}?type=official")
-    #@results1 = JSON.parse(response.body)
-end
-
+def self.search_result(lat,lng)
+  	response = HTTParty.get("http://protectedplanet.net/api2/sites/search_by_point/#{lat}/#{lng}?type=official")
+  end
 end
