8795d8700bbe154a27f239a92911365f86752116
James Cox <th3james@gmail.com>
2013-08-27 11:13:22 +0100

    Make the form page load when given no parameters

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,13 +4,19 @@ class SitesController < ApplicationController
   require 'PPApi'
  
   def index
-    response = HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address=Quito&sensor=false")
-    @response_body = response
-    results = response["results"].first
-    geometry = results["geometry"]
-    location = geometry["location"]
-    lat = location["lat"].to_i
-    lng = location["lng"].to_i
-    @search_results=PPApi.search_result(lat,lng)
+    # If the user has submitted a query
+    if params[:codigo].present?
+      response = HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{params[:codigo]}&sensor=false")
+      @response_body = response
+      results = response["results"].first
+      geometry = results["geometry"]
+      location = geometry["location"]
+      lat = location["lat"].to_i
+      lng = location["lng"].to_i
+      @search_results=PPApi.search_result(lat,lng)
+      puts @search_results
+    else
+      @search_results = []
+    end
   end
 end
