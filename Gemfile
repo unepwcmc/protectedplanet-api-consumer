8a79a509c25cd15bc3a4ea058a0ca83a46d74920
paolamastaiza@gmail.com <paolamastaiza@gmail.com>
2013-08-28 10:47:05 +0100

    simple styling and task 1 terminated

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem 'httparty'
-gem 'pry', group: [:development]
+gem 'bootstrap-sass'
+
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,6 +4,7 @@ class SitesController < ApplicationController
   require 'PPApi'
  
   def index
+    if params[:codigo].present?
     response = HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{params[:codigo]}&sensor=false")
     @response_body = response
     results = response["results"].first
@@ -12,5 +13,9 @@ class SitesController < ApplicationController
     lat = location["lat"].to_i
     lng = location["lng"].to_i
     @search_results=PPApi.search_result(lat,lng)
+    puts @search_results
+    else
+     @search_results = []
+    end
   end
 end
