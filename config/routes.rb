a388152a8d1fd4a5188b4fd04ad2ed403fb8d74e
Patricia Cupueran <pa1809ty@gmail.com>
2013-09-04 20:02:14 +0100

    Root redirected to sites/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Webpage::Application.routes.draw do
-  root "sitesController#index"
+  root "SitesController#index"
   get "sites/index"
   
   # The priority is based upon order of creation: first created -> highest priority.
