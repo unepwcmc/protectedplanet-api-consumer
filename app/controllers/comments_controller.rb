f1a0cf275b6b95186cd773025b4b2e4ef6b7025e
paolamastaiza@gmail.com <paolamastaiza@gmail.com>
2013-09-28 13:07:43 +0100

    trying comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+
+	def create
+    @comment = Comment.new(params[:comment])
+ 
+    if @comment.save
+      ;flash[:notice] = 'Comment was successfully created.'
+      redirect_to(@comment.site)
+    else
+      flash[:notice] = "Error creating comment: #{@comment.errors}"
+      redirect_to(@comment.site)
+    end
+  end
+ 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+ 
+    redirect_to(@comment.site)
+  end
+	
 end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -25,6 +25,7 @@ class SitesController < ApplicationController
     site = PPApi.get_site(@id)
     @name = site["official"]["NAME"]
     @url = "http://protectedplanet.net/sites/#{@id}"
+    @comment = Comment.new( :site => site)
   end
 end
 
